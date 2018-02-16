3cc435bdac6a0c585c8e5aa501cd85fb69efee70
Sarfaraz <nsarfaraz@zeomega.com>
2018-02-16 12:18:21 +0530

    About Dictionaries in Python.

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries basic fundamentals:
+# Dictionaries are the Python's Hash Table Type whcih consists key:value pair.
+# Keys in Dictionaries can be almost any Python Type, but are usually numbers and string.
+# Values on the other hand can be any arbitrary Python object.
+
+aDict = {}                                      #Dictionary declaration
+
+aDict = {'Name' : 'Hunter', 'Age' : 23 }        #Dictionary defination, Here Name and Age are keys
+                                                #Hunter and 23 are Values for the respective keys.
+
+aDict.keys()                                    #Fetching the Keys present in the Dictionary
+
+aDict['Name']                                   #Fetching the value for key Name which will return Hunter
