22bac4569b86ed919c64911be3ebff52bf81e5ee
Tanweer123 <alam.tanweer153@gmail.com>
2019-01-17 01:04:53 +0530

    faulty calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#FAULTY CALCULATOR
+
+print("Ener the first number")
+num1 = int(input())
+print("Enter the second number")
+num2 = int(input())
+print("Enter the operator in order to perform the operation")
+op = input()
+if(num1==45 and num2==3 and op=='*'):
+    result = 555
+elif(num1==56 and num2==9 and op=='+'):
+    result = 77
+elif(num1==56 and num2==6 and op=='/'):
+    result = 4
+else:
+    if op == '+':
+        result=num1+num2
+    elif op == '-':
+        result=num1-num2
+    elif op == '*':
+        result = num1*num2
+    elif op == '/':
+        result = num1/num2
+    else:
+        print("Enter the correct values")
+print("The result is: ", result)
